3852179566dc26e647796dc1ff4c20df8355a21a
Osman Ugur Demirer <osmanugurdemirer@outlook.com>
2023-06-26 02:33:11 +0300

    models are created.

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import project
+from .models import project, About , Service , Portfolio, Comment, Blog
 # Register your models here.
 admin.site.register(project)
+admin.site.register(About)
+admin.site.register(Service)
+admin.site.register(Portfolio)
+admin.site.register(Comment)
+admin.site.register(Blog)
--- app/models.py
+++ app/models.py
@@ -9,3 +9,54 @@ class project(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
     image = models.ImageField()
+    
+    
+class About(models.Model):
+    
+    def __str__(self) -> str:
+        return self.header
+    
+    header = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    icon = models.TextField(max_length=50)
+    date = models.DateField()
+    
+class Service(models.Model):
+    
+    def __str__(self) -> str:
+        return self.header
+    
+    header = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    icon = models.TextField(max_length=50)
+    link = models.URLField(100)
+    
+class Portfolio(models.Model):
+    
+    def __str__(self) -> str:
+        return self.header
+    
+    header = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    image = models.ImageField()
+    
+    
+class Comment(models.Model):
+    
+    def __str__(self) -> str:
+        self.name
+    
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.CharField(max_length=300)
+    
+class Blog(models.Model):
+    
+    def __str__(self) -> str:
+        return self.header
+    
+    header = models.CharField(max_length=50)
+    subHeader = models.CharField(max_length=30)
+    description = models.TextField(max_length=200)
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    
