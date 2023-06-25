6ebbe793644aff42ebe340c691d1cb9a27c78562
Osman Ugur Demirer <osmanugurdemirer@outlook.com>
2023-06-26 00:18:31 +0300

    project class added to admin page.

--- app/models.py
+++ app/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class project(models.Model):
+    
+    def __str__(self) -> str:
+        return self.name
+    
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
     image = models.ImageField()
