916e508e4e57f7276b3ae7a148b3dc3531e259d4
Osman Ugur Demirer <osmanugurdemirer@outlook.com>
2023-06-26 01:04:21 +0300

    urls are added to index page.

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
+    path("services/", views.services, name="services"),
+    path("portfolio/", views.portfolio, name="portfolio"),
+    path("blog/", views.blog, name="blog"),
+    path("contact/", views.contact, name="contact"),
 ]
--- app/views.py
+++ app/views.py
@@ -7,3 +7,15 @@ def index(request):
 
 def about(request):
     return render(request,'about.html')
+
+def services(request):
+    return render(request,'services.html')
+
+def portfolio(request):
+    return render(request,'portfolio.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def contact(request):
+    return render(request,'contact.html')
